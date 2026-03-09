b0227cdcc0e00f11c3c8ee453ec68d5c5d4df63a
Jarbrant <andersmenyit@gmail.com>
2026-03-09 17:39:59 +0100

    Create Admin component for admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,1 +1,23 @@
+export default function Admin(){
 
+ return (
+
+  <div className="p-10">
+
+   <h1 className="text-3xl font-bold">
+    CateringCare Admin
+   </h1>
+
+   <ul className="mt-6">
+
+    <li>Redigera FAQ</li>
+    <li>Se statistik</li>
+    <li>Lägg till frågor</li>
+
+   </ul>
+
+  </div>
+
+ );
+
+}
