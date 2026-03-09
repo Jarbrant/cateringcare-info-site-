6f15214a45d086a858591b673a0f609cdb237b1d
Jarbrant <andersmenyit@gmail.com>
2026-03-09 17:24:58 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,35 @@
+/**
+ * Next.js configuration
+ * Security headers included
+ */
 
+const securityHeaders = [
+  {
+    key: "X-Frame-Options",
+    value: "DENY"
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff"
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin"
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block"
+  }
+];
+
+module.exports = {
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders
+      }
+    ];
+  }
+};
