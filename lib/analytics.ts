ee13568fae3883ca1f627a072c7b9e32994fe2f4
Jarbrant <andersmenyit@gmail.com>
2026-03-09 17:38:58 +0100

    Implement question logging functionality
    Added a function to log questions to a file.

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,1 +1,14 @@
+/**
+ simple analytics
+ stores asked questions
+*/
 
+import fs from "fs";
+
+const LOG = "questions.log";
+
+export function logQuestion(question:string){
+
+ fs.appendFileSync(LOG, question + "\n");
+
+}
