3bcd7acab7be2b82f7518a75fbb88e76b3bfaeac
Jarbrant <andersmenyit@gmail.com>
2026-03-09 17:32:56 +0100

    Create FAQItem component for displaying questions
    Added an accessible accordion component for FAQs.

--- components/FAQItem.tsx
+++ components/FAQItem.tsx
@@ -1,1 +1,27 @@
+/**
+ * Accessible accordion component
+ */
 
+export default function FAQItem({
+  question,
+  answer
+}: {
+  question: string,
+  answer: string
+}) {
+
+  return (
+
+    <details className="border rounded p-4 mb-3">
+
+      <summary className="font-semibold cursor-pointer">
+        {question}
+      </summary>
+
+      <p className="mt-2 text-gray-700">
+        {answer}
+      </p>
+
+    </details>
+  );
+}
