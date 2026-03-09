632e9e92e0bb45e90b89f6282d9616d7d56e0a65
Jarbrant <andersmenyit@gmail.com>
2026-03-09 18:26:40 +0100

    Update next.config.js with new configuration options

--- next.config.js
+++ next.config.js
@@ -2,26 +2,26 @@
 
 const nextConfig = {
 
+  output: "export",
+
+  basePath: "/cateringcare-info-site-",
+
+  images: {
+    unoptimized: true,
+  },
+
   async headers() {
-
     return [
-
       {
         source: "/(.*)",
-
         headers: [
-
           { key: "X-Frame-Options", value: "DENY" },
           { key: "X-Content-Type-Options", value: "nosniff" },
-          { key: "X-XSS-Protection", value: "1; mode=block" }
+          { key: "X-XSS-Protection", value: "1; mode=block" },
+        ],
+      },
+    ];
+  },
+};
 
-        ]
-      }
-
-    ]
-
-  }
-
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
